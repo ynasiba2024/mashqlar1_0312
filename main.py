d0af3b312d8168242b7ca8866d6765a98dfc1799
ynasiba2024 <t7979176@gmail.com>
2025-12-03 16:13:40 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+# 1-misol
+yosh = int(input("Yoshingizni kiriting: "))
+
+if yosh >= 18:
+    print("Kattasiz")
+else:
+    print("Kattasiz emas")
+
+
+# 2-misol
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+if a > b:
+    print("Birinchi son katta")
+else:
+    print("Ikkinchi son katta")
+
+
+#3-misol
+son = int(input("Son kiriting: "))
+
+if son > 0:
+    print("Musbat son")
+else:
+    print("Manfiy son")
+
+# 4--misol
+raqam = int(input("Raqam kiriting: "))
+
+if raqam < 10:
+    print("10 dan kichik")
+elif raqam == 10:
+    print("10 ga teng")
+else:
+    print("10 dan katta")
+
+#5-misol
+a = int(input("1-son: "))
+b = int(input("2-son: "))
+
+if a == b:
+    print("Sonlar teng")
+else:
+    print("Sonlar teng emas")
+
+# 6-misol
+matn = input("So'z kiriting: ")
+
+if matn == "hello":
+    print("Salom!")
+elif matn == "bye":
+    print("Xayr!")
+
+
